35fa42e5acae9f44f9e1cda24912ef774a98a8dc
Goran Mahovlic <goran.mahovlic@gmail.com>
2022-11-18 16:51:21 +0100

    Adding prepare.sh and gdew042 sample

---
+++ prepare.sh
@@ -0,0 +1,3 @@
+# Copy just CMakeLists.txt so I do not need to fork CalEPD
+echo "Copying CMakeLists.txt to CalEPD folder so I do not need to fork that repository"
+cp config/CalEPD_CMakeLists.txt components/CalEPD/CMakeLists.txt
