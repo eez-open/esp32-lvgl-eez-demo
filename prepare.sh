580d6383ab23758d20121de6bb45036e5b97dd73
Goran Mahovlic <goran.mahovlic@gmail.com>
2022-11-18 17:29:29 +0100

    Fixsing prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,3 +1,3 @@
 # Copy just CMakeLists.txt so I do not need to fork CalEPD
 echo "Copying CMakeLists.txt to CalEPD folder so I do not need to fork that repository"
-cp config/CalEPD_CMakeLists.txt components/CalEPD/CMakeLists.txt
+cp configs/CalEPD_CMakeLists.txt components/CalEPD/CMakeLists.txt
