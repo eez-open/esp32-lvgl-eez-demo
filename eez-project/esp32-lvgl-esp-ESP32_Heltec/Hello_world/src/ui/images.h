6baccdc79f84019935b48ad05f4db43568296385
Goran Mahovlic <goran.mahovlic@gmail.com>
2022-11-19 19:11:25 +0100

    Adding more sdkconfigs

---
+++ eez-project/esp32-lvgl-esp-ESP32_Heltec/Hello_world/src/ui/images.h
@@ -0,0 +1,15 @@
+#ifndef EEZ_LVGL_UI_IMAGES_H
+#define EEZ_LVGL_UI_IMAGES_H
+
+#include "lvgl/lvgl.h"
+
+
+typedef struct _ext_img_desc_t {
+    const char *name;
+    const lv_img_dsc_t *img_dsc;
+} ext_img_desc_t;
+
+extern const ext_img_desc_t images[0];
+
+
+#endif /*EEZ_LVGL_UI_IMAGES_H*/
