9aa411cfb3d1a7cf494cf38536acafed900305aa
Martin Vladic <martin.vladic@gmail.com>
2022-11-20 19:21:14 +0100

    simpler solution for inclusion of eez-framework

--- main/ui/images.h
+++ main/ui/images.h
@@ -4,10 +4,13 @@
 #include "lvgl/lvgl.h"
 
 
+#ifndef EXT_IMG_DESC_T
+#define EXT_IMG_DESC_T
 typedef struct _ext_img_desc_t {
     const char *name;
     const lv_img_dsc_t *img_dsc;
 } ext_img_desc_t;
+#endif
 
 extern const ext_img_desc_t images[0];
 
---
+++ main/ui/ui.h
@@ -0,0 +1,9 @@
+#ifndef EEZ_LVGL_UI_GUI_H
+#define EEZ_LVGL_UI_GUI_H
+
+#include <eez/flow/lvgl_api.h>
+
+void ui_init();
+void ui_tick();
+
+#endif // EEZ_LVGL_UI_GUI_H
