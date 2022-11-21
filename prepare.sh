fa19eaa34aa6c94fdb8318037d82daa689583a7e
Martin Vladic <martin.vladic@gmail.com>
2022-11-21 14:38:26 +0100

    Delete prepare.sh

--- prepare.sh
+++
@@ -1,3 +0,0 @@
-# Copy just CMakeLists.txt so I do not need to fork CalEPD
-echo "Copying CMakeLists.txt to CalEPD folder so I do not need to fork that repository"
-cp configs/CalEPD_CMakeLists.txt components/CalEPD/CMakeLists.txt
